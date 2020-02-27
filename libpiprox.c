fc7df73abbf4adfd4f407ba538b41ddcc8480595
Patrick Karnia <pkarnia@iit.edu>
2020-02-27 15:20:01 -0600

    add missing ;

--- libpiprox.c
+++ libpiprox.c
@@ -150,7 +150,7 @@ Returns 1 on success, 0 if the data stored within the internal state is too smal
 */
 int piprox_hidH10301_parse(piprox_state_t *st, piprox_hidH10301_t *res){
     int i,f = 0;
-    uint8_t paritya=0,parityb=0
+    uint8_t paritya=0,parityb=0;
     if(st->card_data_len<4){
         return 0;
     }
